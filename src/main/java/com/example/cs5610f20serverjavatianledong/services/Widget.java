44930ac2e95a863212391c9b0b882163e05483c3
Tianle Dong <dtllawrence@gmail.com>
2020-10-24 23:47:12 -0400

    created Widget.java

---
+++ src/main/java/com/example/cs5610f20serverjavatianledong/services/Widget.java
@@ -0,0 +1,12 @@
+package com.example.cs5610f20serverjavatianledong.services;
+
+public class Widget {
+  private String name;
+  private Integer id;
+  private String type;
+  private Integer widgetOrder;
+  private String text;
+  private String src;
+  private Integer size;
+
+}
